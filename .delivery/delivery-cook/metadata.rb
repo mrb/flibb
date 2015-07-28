3e6ca65ec2edc65934587547452f72f7ea9b4bd7
Sean OMeara <sean@chef.io>
2015-07-28 11:54:45 -0400

    renaming to delivery-cook

---
+++ .delivery/delivery-cook/metadata.rb
@@ -0,0 +1,13 @@
+name             'delivery-cook'
+maintainer       'Sean OMeara'
+maintainer_email 'sean@chef.io'
+license          'Apache 2.0'
+description      'Delivery build cookbook for chef-cookbooks!'
+
+version          '0.1.0'
+
+supports 'ubuntu', '>= 14.04'
+supports 'redhat', '>= 6.5'
+supports 'centos', '>= 6.5'
+
+depends 'docker'
